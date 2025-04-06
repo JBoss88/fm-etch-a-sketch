59886e1498a9db7ada79bbec53740890c6df52e7
Jackson <jacksonboss88@gmail.com>
2025-04-06 13:20:13 -0500

    index.html

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 'use strict';
 
 const container = document.getElementById('gridContainer');
-const clickMe = document.querySelector('.clickMe');
+const btn = document.querySelector('button');
 
-for (let i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < 2 * 2; i++) {
   let box = document.createElement('div');
   box.classList.add('box');
   container.append(box);
 }
 
+const createColor = () => {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
 container.addEventListener('mouseover', e => {
   const clicked = e.target.closest('.box');
   if (!clicked) return;
-  clicked.style.backgroundColor = 'green';
+  clicked.style.backgroundColor = createColor();
 });
 
 const getAlertInput = () => {
   const gridSize = +prompt('How many squares per side would you like?');
+  const containerHeight = container.clientHeight;
+  const containerWidth = container.clientWidth;
 
-  if (gridSize === null) {
-    return;
-  } else if (gridSize <= 100) {
+  const calcGrid = () => {
+    let newBox = document.createElement('div');
+    newBox.classList.add('new-box');
+    let newHeight = containerHeight / gridSize / 10;
+    let newWidth = containerWidth / gridSize / 10;
+
+    newBox.style.height = `${newHeight}rem`;
+    newBox.style.width = `${newWidth}rem`;
+    container.append(newBox);
+  };
+
+  if (gridSize <= 100) {
     container.innerHTML = '';
     for (let j = 0; j < gridSize * gridSize; j++) {
-      let box = document.createElement('div');
-      box.classList.add('box');
-      container.append(box);
+      calcGrid();
     }
   }
 };
+btn.addEventListener('click', getAlertInput);
 
-clickMe.addEventListener('click', getAlertInput);
+container.addEventListener('mouseover', e => {
+  const clicked = e.target.closest('.new-box');
+  if (!clicked) return;
+  clicked.style.backgroundColor = createColor();
+});
